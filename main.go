9de3977c7ce2577aefd814c8ac850df77371ca58
agincgit <45492083+agincgit@users.noreply.github.com>
2025-06-01 22:12:41 -0500

    Apply gofmt formatting

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "time"
+	"time"
 
-    log "github.com/sirupsen/logrus"
-    "github.com/agincgit/forgeworker/config"
-    "github.com/agincgit/forgeworker/service"
+	"github.com/agincgit/forgeworker/config"
+	"github.com/agincgit/forgeworker/service"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-    cfg := config.GetConfig("config.json")
+	cfg := config.GetConfig("config.json")
 
-    workerID, err := service.RegisterWorker(cfg)
-    if err != nil {
-        log.Fatalf("Worker registration failed: %v", err)
-    }
+	workerID, err := service.RegisterWorker(cfg)
+	if err != nil {
+		log.Fatalf("Worker registration failed: %v", err)
+	}
 
-    service.StartHeartbeatScheduler(cfg, workerID, 30*time.Second)
-    service.StartWorkerLoop(cfg, workerID)
+	service.StartHeartbeatScheduler(cfg, workerID, 30*time.Second)
+	service.StartWorkerLoop(cfg, workerID)
 }
